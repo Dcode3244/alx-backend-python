cd9ad382cdbd121fdf0f479fa1bbbb5cff9e7e21
Dcode3244 <dagimourgie111@gmail.com>
2023-05-02 06:04:11 +0000

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+takes a string str1 and string str2 as arguments
+and returns a concatenates string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ returns concatenated string (str1 + str2) """
+    return str1 + str2
