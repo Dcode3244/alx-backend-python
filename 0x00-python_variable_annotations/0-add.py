85710a303a2ca10e46684750cf4f2f015f98ce26
Dcode3244 <dagimourgie111@gmail.com>
2023-05-02 05:58:37 +0000

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+contains function that takes a float a and a float b
+as argument and returns their sum as a float
+'''
+
+
+def add(a: float, b: float) -> float:
+    """ returns sum of a and b as a float """
+    return a + b
