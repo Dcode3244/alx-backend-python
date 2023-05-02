67712b4f7cd915316355d3ac4d7582d9706ca10b
Dcode3244 <dagimourgie111@gmail.com>
2023-05-02 06:16:28 +0000

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+contains a function that takes a float as an argument
+and returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """ returns the string representation of n """
+    return str(n)
